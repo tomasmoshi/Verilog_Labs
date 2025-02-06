60224270ce8ea67c3da889f1e0d12bbf6ad2b828
Aram Petrosyan <142950097+aarampetrosyann@users.noreply.github.com>
2025-02-06 12:05:32 -0800

    added test cases

--- tb/FPCVT_tb.v
+++ tb/FPCVT_tb.v
@@ -10,6 +10,22 @@ module FPCVT_tb;
 	wire [2:0] E;
 	wire [3:0] F;
 
+    fpcvt uut (
+        .D(D),
+        .S(S),
+        .E(E),
+        .F(F)
+    );
 
+    initial begin
+        D = 12'b011111111111;
+        #100;
+        D = 12'b000101010101;
+        #100;
+        D = 12'b000001101001;
+        #100;
+        D = 12'b000001111101;
+        #100;
+    end
 
 endmodule
