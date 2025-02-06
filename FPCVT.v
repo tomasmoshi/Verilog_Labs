2eca54affbb3f0bc627d20a4c3e4bbe058deb114
tomasmoshi <tomas.alosh@gmail.com>
2025-02-06 11:44:18 -0800

    round.v

--- FPCVT.v
+++ FPCVT.v
@@ -1,18 +1,27 @@
-module FPCVT_Pin_Descriptions (
-);
-    input [11:0] D, //12 bit 
-    output reg S, //sign conversion
-    output reg [2:0] E, //12
-    output reg [3:0] F.
-    //wires
-    reg [11:0] signedMagnitude;
-    reg [3:0] significand_temp;
-    reg [3:0] round_bit;
-    integer  leading_zero;
+`timescale 1ns / 1ps
+module FPCVT(D, S, E, F);
 
-    
+	// Inputs
+	input [11:0] D;
 
+	// Outputs
+	output S;
+	output [2:0] E;
+	output [3:0] F;
 
+	// Wires
+	wire [11:0] signed_mag;
+	wire fifth_bit;
+	wire [2:0] exp;
+	wire [3:0] sig;
 
+	// Convert D from Two's Complement to Signed Magnitude
+	to_signed_magnitude m1 (.D(D), .S(S), .signed_mag(signed_mag));
+
+	// Count leading zeroes and extract leading bits
+	leading_zeroes m2 (.D(signed_mag), .E(exp), .F(sig), .fifth_bit(fifth_bit));
+
+	// Round
+	handle_round m3 (.E_in(exp), .F_in(sig), .fifth_bit(fifth_bit), .E_out(E), .F_out(F));
 
 endmodule
