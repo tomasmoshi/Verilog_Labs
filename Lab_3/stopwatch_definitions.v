1b5c789b32a966f3e1506a8265402d431a20e131
Aram Petrosyan <142950097+aarampetrosyann@users.noreply.github.com>
2025-02-22 18:14:41 -0800

    added stopwatch_definitions.v

---
+++ Lab_3/stopwatch_definitions.v
@@ -0,0 +1,29 @@
+// Cathode values
+parameter ST_0000 = 7'b1000000;
+parameter ST_0001 = 7'b1111001;
+parameter ST_0010 = 7'b0100100;
+parameter ST_0011 = 7'b0110000;
+parameter ST_0100 = 7'b0011001;
+parameter ST_0101 = 7'b0010010;
+parameter ST_0110 = 7'b0000010;
+parameter ST_0111 = 7'b1111000;
+parameter ST_1000 = 7'b0000000;
+parameter ST_1001 = 7'b0010000;
+parameter ST_OFF = 7'b1111111;
+
+// Anode values
+parameter MINUTES_TENS_DIGIT = 4'b0111;
+parameter MINUTES_ONES_DIGIT = 4'b1011;
+parameter SECONDS_TENS_DIGIT = 4'b1101;
+parameter SECONDS_ONES_DIGIT = 4'b1110;
+
+// Limits
+parameter MAX_ONES = 9;
+parameter MAX_TENS = 5;
+
+// Counters
+parameter TWO_HZ_COUNT = 25000000;
+parameter ONE_HZ_COUNT = 50000000;
+parameter FAST_COUNT = 200000;
+parameter BLINK_COUNT = 37500000;
+parameter DB_COUNT = 65535;
